1082be2f76ac6465f97e91ac484ecb3586a40e96
Renrui Zhang <54577425+ZrrSkywalker@users.noreply.github.com>
2023-03-16 23:10:42 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,3 +10,27 @@ def cls_acc(output, target, topk=1):
     acc = float(correct[: topk].reshape(-1).float().sum(0, keepdim=True).cpu().numpy())
     acc = 100 * acc / target.shape[0]
     return acc
+
+
+def compute_overall_iou(pred, target, num_classes=50):
+    shape_ious = []
+    pred = pred.max(dim=2)[1]   # (batch_size, num_points)  the pred_class_idx of each point in each sample
+    pred_np = pred.cpu().data.numpy()
+
+    target_np = target.cpu().data.numpy()
+    
+    for shape_idx in range(pred.shape[0]):   # sample_idx
+        part_ious = []
+        for part in range(num_classes):   # class_idx! no matter which category, only consider all part_classes of all categories, check all 50 classes
+            # for target, each point has a class no matter which category owns this point! also 50 classes!!!
+            # only return 1 when both belongs to this class, which means correct:
+            I = np.sum(np.logical_and(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+            # always return 1 when either is belongs to this class:
+            U = np.sum(np.logical_or(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+            F = np.sum(target_np[shape_idx] == part)
+
+            if F != 0:       
+                iou = I / float(U)    #  iou across all points for this class
+                part_ious.append(iou)   #  append the iou of this class
+        shape_ious.append(np.mean(part_ious))   # each time append an average iou across all classes of this sample (sample_level!)
+    return shape_ious   # [batch_size]
